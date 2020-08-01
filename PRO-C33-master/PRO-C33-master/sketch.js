6cb5fb8aca1d569b9db4cf42fb1ba23524440d1c
weirdocorn <66620244+weirdocorn@users.noreply.github.com>
2020-08-01 19:36:46 +0530

    Add files via upload

---
+++ PRO-C33-master/PRO-C33-master/sketch.js
@@ -0,0 +1,105 @@
+var gamestate = play;
+
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+
+var divisionHeight = 300;
+
+var score = 0;
+textSize(2);
+text("score = "+score, 80, 30);
+var particle;
+var turn = 0;
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  for (var k = 0; k <=width; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+    text(100, k, height-divisionHeight/2);
+  }
+
+  if(plinkos.x === k && plinkos.y === height-divisionHeight/2){
+    score = score+100;
+    particle = null;
+  }
+
+
+  for (var j = 75; j <=width; j=j+50) {
+    plinkos.push(new Plinko(j,75));
+  }
+
+  for (var j = 50; j <=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  for (var j = 75; j <=width; j=j+50) {
+    plinkos.push(new Plinko(j,275));
+  }
+
+  for (var j = 50; j <=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,375));
+  }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
+
+function mousePressed(){
+  if(gamestate !== "end"){
+    count++;
+    particle = new Particle(mouseX, 10, 10, 10)
+  }
+}
+
+function eachTurn(){
+  if(score = score+100){
+    turn = turn+1
+  }
+}
+
+function endTheGame(){
+  if(turn === 5){
+    gamestate = end;
+    textSize(5);
+    text("Game Over", 400, 400);
+  }
+}
